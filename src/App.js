59389f423d484fdd79b516037a4ddfa205d0395d
misha novakishvili <46324818+misza777@users.noreply.github.com>
2022-04-20 00:24:28 +0200

    setting up css

--- src/App.js
+++ src/App.js
@@ -1,24 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useState, useEffect } from "react";
 
 function App() {
+  //state
+  const [questions, setQuestions] = useState([]);
+
+  const createNewQuestions = () => {};
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      {questions.length > 0 ? (
+        {
+          /* <Split sizes={[30, 70]} direction="horizontal" className="split">
+          <Sidebar
+            notes={notes}
+            currentNote={findCurrentNote()}
+            setCurrentNoteId={setCurrentNoteId}
+            newNote={createNewNote}
+            deleteNote={deleteNote}
+          />
+          {currentNoteId && notes.length > 0 && (
+            <Editor currentNote={findCurrentNote()} updateNote={updateNote} />
+          )}
+        </Split> */
+        }
+      ) : (
+        <div className="no-notes">
+          <h1>Quizzical</h1>
+          <h4>Let's have fun with small quizz!</h4>
+          <button className="first-note" onClick={createNewQuestions}>
+            Start quizz now!
+          </button>
+        </div>
+      )}
+    </main>
   );
 }
 
